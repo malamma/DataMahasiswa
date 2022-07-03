ed7ceb0498338b1de68d00e9f156d18b6063cca6
malamma <achmadfauzi22.af@gmail.com>
2022-07-03 22:44:14 +0700

    menambahkan model dan menambahkan routing tambah data

--- resources/views/mahasiswa/menu.blade.php
+++ resources/views/mahasiswa/menu.blade.php
@@ -1,34 +1,31 @@
 @extends('layouts.mainLayout')
 
 @section('mainContent')
-<a class="btn btn-primary" href="/tambahData">Tambah Data</a>
+    <a class="btn btn-primary" href="/tambahData">Tambah Data</a>
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">NIM</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Prodi</th>
+                <th scope="col">Action</th>
+
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            @foreach ($dataMhs as $item)
+                <tr>
+                    <td>{{ $item->nim }}</td>
+                    <td>{{ $item->nama }}</td>
+                    <td>{{ $item->jenisKelamin }}</td>
+                    <td>{{ $item->prodi }}</td>
+                    <td><a class="btn btn-primary" href="/tambahData">Edit Data</a>
+                        <a class="btn btn-primary" href="/tambahData">Hapus Data</a>
+                    </td>
+                </tr>
+            @endforeach
+
         </tbody>
     </table>
 @endsection
